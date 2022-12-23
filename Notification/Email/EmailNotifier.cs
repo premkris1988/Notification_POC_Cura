1a544b907268d986098e24a42e4410d33a8cb873
Prem Krishan <Prem.Krishan@motor.com>
2022-12-23 16:51:39 +0530

    emply ;

--- Notification/Email/EmailNotifier.cs
+++ Notification/Email/EmailNotifier.cs
@@ -12,7 +12,7 @@ namespace Notification.Email
             var client = new SendGridClient(sgKey);
             var from = new EmailAddress(fromEmail, fromAlias);
             var subject = message.Title;
-            var tos = message.ToAddresses.Select(i => new EmailAddress(i)).ToList(); ;
+            var tos = message.ToAddresses.Select(i => new EmailAddress(i)).ToList();
             var plainTextContent = message.Body;
             var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, tos, subject, plainTextContent, string.Empty);
             var response = await client.SendEmailAsync(msg);
